8c2181ea7d684f7d273838a216a86615e98b7253
erfan.mehraban <erfan.mehraban@gmail.com>
2018-04-01 17:54:33 +0430

    source languag added

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 result_format = "{src},{dest}\r\n"
+src_lang = "en"
 dest_lang = "fa"
 input_file_name = 'test_input'
 ouput_file_name = 'test_output.txt'
--- flashcard_generator.py
+++ flashcard_generator.py
@@ -3,8 +3,8 @@ from config import *
 
 translator = Translator()
 all_words = open(input_file_name).read().split()
-ouput_file = open(ouput_file_name, 'a')
-translated_words = translator.translate(all_words, dest=dest_lang)
+ouput_file = open(ouput_file_name, 'w')
+translated_words = translator.translate(all_words, dest=dest_lang, src=src_lang)
 for word in translated_words:
     ouput_file.write(result_format.format_map({
         "src" : word.origin,
