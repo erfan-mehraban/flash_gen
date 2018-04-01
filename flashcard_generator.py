7bff4c8bd2c3eaeec498a7e8b386eeb772c7a824
erfan.mehraban <erfan.mehraban@gmail.com>
2018-04-01 18:06:54 +0430

    removing last empty line from output

--- flashcard_generator.py
+++ flashcard_generator.py
@@ -3,10 +3,12 @@ from config import *
 
 translator = Translator()
 all_words = open(input_file_name).read().split()
-ouput_file = open(ouput_file_name, 'w')
 translated_words = translator.translate(all_words, dest=dest_lang, src=src_lang)
+result = ""
 for word in translated_words:
-    ouput_file.write(result_format.format_map({
+    result += result_format.format_map({
         "src" : word.origin,
         "dest": word.text
-    }))
+    })
+ouput_file = open(ouput_file_name, 'w')
+ouput_file.write(result[:-2])
